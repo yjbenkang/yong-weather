03f59c8c4ab976a52f0de61ede255862b5703c9d
강용진 <kyongjin1234@gmail.com>
2021-08-30 16:31:22 +0900

    3.2 아이콘 및 스타일링

--- Weather.js
+++ Weather.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export const Weather = ({temp, condition}) => {
     return (
         <View style={styles.container}>
-          <Text>{temp}</Text>
-          <Text>{condition}</Text>
+        <View style={styles.halfContainer}>
+            <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+            <Text style={styles.temp}>{temp}℃ </Text>
+        </View>
+        <View style={styles.halfContainer} />
         </View>
     );
 };
@@ -29,6 +32,14 @@ Weather.propTypes = {
   
   const styles = StyleSheet.create({
     container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    temp: {
+      fontSize: 42
+    },
+    halfContainer: {
       flex: 1,
       justifyContent: "center",
       alignItems: "center"
