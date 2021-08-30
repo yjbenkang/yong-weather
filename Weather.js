641529df98278626483c45df98ba167fe810b40c
강용진 <kyongjin1234@gmail.com>
2021-08-30 18:56:56 +0900

    Demo Version 1.0

--- Weather.js
+++ Weather.js
@@ -3,27 +3,37 @@ import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
 import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-
+import { useFonts } from 'expo-font';
 const weatherOptions = {
     Thunderstorm: {
       iconName: "weather-lightning",
-      gradient: ["#373B44", "#4286f4"]
+      gradient: ["#373B44", "#4286f4"],
+      title: "맑음",
+      subtitle: "오늘 같은날 밖은 위험합니다."
     },
     Drizzle: {
-      iconName: "weather-hail",
-      gradient: ["#89F7FE", "#66A6FF"]
+      iconName: "weather-rainy",
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "이슬비",
+      subtitle: "잔잔하게 비옵니다."
     },
     Rain: {
-      iconName: "weather-rainy",
-      gradient: ["#00C6FB", "#005BEA"]
+      iconName: "weather-pouring",
+      gradient: ["#00C6FB", "#005BEA"],
+      title: "비",
+      subtitle: "우산을 챙깁시다."
     },
     Snow: {
       iconName: "weather-snowy",
-      gradient: ["#7DE2FC", "#B9B6E5"]
+      gradient: ["#7DE2FC", "#B9B6E5"],
+      title: "눈",
+      subtitle: "이제는 눈이 싫은 나이"
     },
     Atmosphere: {
       iconName: "weather-hail",
-      gradient: ["#89F7FE", "#66A6FF"]
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "맑음",
+      subtitle: "밖에 나가자~ : )"
     },
     Clear: {
       iconName: "weather-sunny",
@@ -33,21 +43,27 @@ const weatherOptions = {
     },
     Clouds: {
       iconName: "weather-cloudy",
-      gradient: ["#D7D2CC", "#304352"]
+      gradient: ["#D7D2CC", "#304352"],
+      title: "흐림",
+      subtitle: "우리 인생은 맑음"
     },
     Mist: {
-      iconName: "weather-hail",
-      gradient: ["#4DA0B0", "#D39D38"]
+      iconName: "weather-fog",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "물안개",
+      subtitle: "안전운전!"
     },
     Dust: {
-      iconName: "weather-hail",
-      gradient: ["#4DA0B0", "#D39D38"]
+      iconName: "weather-hazy",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "미세먼지 많음",
+      subtitle: "마스크 필수"
     },
     Haze: {
-      iconName: "weather-hail",
+      iconName: "weather-hazy",
       gradient: ["#4DA0B0", "#D39D38"],
-      title: "Haze",
-      subtitle: "Just don't go outside."
+      title: "안개",
+      subtitle: "먼지로 인한 안개 !!"
     }
   };
 
@@ -59,6 +75,7 @@ export const Weather = ({
     tempMax,
     tempMin,
     windSpeed }) => {
+
     return (
         <LinearGradient
           colors={weatherOptions[condition].gradient}
@@ -117,6 +134,7 @@ Weather.propTypes = {
     },
     temp: {
       fontSize: 42,
+      fontWeight: "600",
       color:"white"
     },
     halfContainer: {
@@ -126,32 +144,30 @@ Weather.propTypes = {
     },
     quaterContainer: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        justifyContent:"center",
+        alignItems:"center"
       },
     samsaContainer: {
       flex: 3,
-      justifyContent: "center",
-      alignItems: "center"
+      justifyContent:"center",
+      alignItems:"center"
     },
     title: {
         color: "white",
         fontSize: 44,
-        fontWeight: "300",
-        marginBottom: 10
+        fontWeight: "800",
+        marginBottom: 10,
     },
     subtitle: {
-        fontWeight: "600",
+        fontWeight: "400",
         color: "white",
-        fontSize: 24
+        fontSize: 20
     },
     description: {
         color: "white",
-        fontSize: 20,
-        marginBottom: 10
+        fontSize: 23,
+        marginBottom: 10,
+        fontWeight: "700"
     },
-    textContainer: {
-        paddingHorizontal: 20,
-        alignItems: "flex-start"
-    }
+
   });
