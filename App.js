48e5e11026c0ca6eb372edb557d943739e5f214a
강용진 <kyongjin1234@gmail.com>
2021-08-30 12:14:14 +0900

    2.4 날씨 가져오기

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
+import axios from "axios";
+
+const API_KEY = "2bd59b60cf5a0c3d72bfc726cf9aeebf";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -18,13 +29,16 @@ export default class extends React.Component {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.setState({ isLoading: false });
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
+  
   componentDidMount() {
     this.getLocation();
   }
+
   render() {
     const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
