1c2c4022eea7b7cd874021f5a6679561197caf60
강용진 <kyongjin1234@gmail.com>
2021-08-30 12:49:39 +0900

    3.1 Getting the Condition Names

--- Weather.js
+++ Weather.js
@@ -2,16 +2,29 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-export const Weather = ({temp}) => {
+export const Weather = ({temp, condition}) => {
     return (
         <View style={styles.container}>
           <Text>{temp}</Text>
+          <Text>{condition}</Text>
         </View>
     );
 };
 
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+      ]).isRequired
   };
   
   const styles = StyleSheet.create({
